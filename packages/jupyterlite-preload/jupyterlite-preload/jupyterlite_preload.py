35fb94879712efdb18890ef68b50c56556510b20
Juniper Tyree <50025784+juntyr@users.noreply.github.com>
2024-10-16 15:42:44 +0000

    Split jupyterlite-preload patches into early and finalize

--- packages/jupyterlite-preload/jupyterlite-preload/jupyterlite_preload.py
+++ packages/jupyterlite-preload/jupyterlite-preload/jupyterlite_preload.py
@@ -1,109 +1,104 @@
-def _patch():
-    if getattr(_patch, "_patched", False):
-        return
-    setattr(_patch, "_patched", True)
+import asyncio
+import importlib
+import sys
+from pathlib import Path
 
-    import asyncio
-    import importlib
-    import sys
-    from pathlib import Path
+import pyodide
+import pyodide_js
 
-    import pyodide
-    import pyodide_js
+import ipyloglite
 
-    from IPython import get_ipython
-
-    import ipyloglite
-
-    import pyodide_http
-    pyodide_http.patch_all()
+import pyodide_http
+pyodide_http.patch_all()
 
 
-    async def asyncio_gather(*coros_or_futures, return_exceptions=False):
-        results = []
+async def asyncio_gather(*coros_or_futures, return_exceptions=False):
+    results = []
 
-        for coro in coros_or_futures:
-            try:
-                results.append(await coro)
-            except Exception as err:
-                if return_exceptions:
-                    results.append(err)
-                else:
-                    raise
+    for coro in coros_or_futures:
+        try:
+            results.append(await coro)
+        except Exception as err:
+            if return_exceptions:
+                results.append(err)
+            else:
+                raise
 
-        return results
-
-
-    # FIXME: somehow detect if we're actually running on a loop and use the actual
-    #        async sleep in that case
-    async def asyncio_sleep(delay, result=None):
-        import time
-        time.sleep(delay)
-        return result
+    return results
 
 
-    asyncio.gather = asyncio_gather
-    asyncio.sleep = asyncio_sleep
+# FIXME: somehow detect if we're actually running on a loop and use the actual
+#        async sleep in that case
+async def asyncio_sleep(delay, result=None):
+    import time
+    time.sleep(delay)
+    return result
 
 
-    async def loadPackagesFromImports(
-        code: str, options=dict(checkIntegrity=True),
-    ):
-        imports = set()
-
-        for name in pyodide.code.find_imports(code):
-            if name in sys.modules:
-                continue
-
-            spec = importlib.util.find_spec(name)
-            if spec is not None and Path(spec.origin).parts[:2] == (
-                "/", "drive",
-            ):
-                with open(spec.origin, "r") as f:
-                    await loadPackagesFromImports(f.read(), options=options)
-
-            if name in pyodide_js._api._import_name_to_package_name:
-                imports.add(
-                    pyodide_js._api._import_name_to_package_name[name]
-                )
-
-        return await pyodide_js.loadPackage(list(imports))
+asyncio.gather = asyncio_gather
+asyncio.sleep = asyncio_sleep
 
 
-    pyodide_js.loadPackagesFromImports = loadPackagesFromImports
+async def loadPackagesFromImports(
+    code: str, options=dict(checkIntegrity=True),
+):
+    imports = set()
+
+    for name in pyodide.code.find_imports(code):
+        if name in sys.modules:
+            continue
+
+        spec = importlib.util.find_spec(name)
+        if spec is not None and Path(spec.origin).parts[:2] == (
+            "/", "drive",
+        ):
+            with open(spec.origin, "r") as f:
+                await loadPackagesFromImports(f.read(), options=options)
+
+        if name in pyodide_js._api._import_name_to_package_name:
+            imports.add(
+                pyodide_js._api._import_name_to_package_name[name]
+            )
+
+    return await pyodide_js.loadPackage(list(imports))
 
 
-    class PyodideMemoryMonitor:
-        def __init__(self):
-            self.update()
-
-        def update(self):
-            self.memory = pyodide_js._module.HEAPU8.length
-
-        # https://stackoverflow.com/a/1094933
-        def sizeof_fmt(self, num, suffix="B"):
-            for unit in ("", "Ki", "Mi", "Gi", "Ti", "Pi", "Ei", "Zi"):
-                if abs(num) < 1024.0:
-                    return f"{num:3.1f}{unit}{suffix}"
-                num /= 1024.0
-            return f"{num:.1f}Yi{suffix}"
+pyodide_js.loadPackagesFromImports = loadPackagesFromImports
 
 
-        def pre_run_cell(self, *args, **kwargs):
-            self.update()
+class PyodideMemoryMonitor:
+    def __init__(self):
+        self.update()
 
-        def post_execute_hook(self, *args, **kwargs):
-            memory_before = self.sizeof_fmt(self.memory)
-            self.update()
-            memory_after = self.sizeof_fmt(self.memory)
+    def update(self):
+        self.memory = pyodide_js._module.HEAPU8.length
 
-            if memory_after != memory_before:
-                print(
-                    f"[pyodide]: Memory usage has grown to {memory_after} " +
-                    f"(from {memory_before}) for this notebook",
-                    file=sys.stderr,
-                )
-    
+    # https://stackoverflow.com/a/1094933
+    def sizeof_fmt(self, num, suffix="B"):
+        for unit in ("", "Ki", "Mi", "Gi", "Ti", "Pi", "Ei", "Zi"):
+            if abs(num) < 1024.0:
+                return f"{num:3.1f}{unit}{suffix}"
+            num /= 1024.0
+        return f"{num:.1f}Yi{suffix}"
+
+
+    def pre_run_cell(self, *args, **kwargs):
+        self.update()
+
+    def post_execute_hook(self, *args, **kwargs):
+        memory_before = self.sizeof_fmt(self.memory)
+        self.update()
+        memory_after = self.sizeof_fmt(self.memory)
+
+        if memory_after != memory_before:
+            print(
+                f"[pyodide]: Memory usage has grown to {memory_after} " +
+                f"(from {memory_before}) for this notebook",
+                file=sys.stderr,
+            )
+
+
+def _finalize_with_ipython(ip):
     monitor = PyodideMemoryMonitor()
-    get_ipython().events.register("pre_run_cell", monitor.pre_run_cell)
-    get_ipython().events.register("post_execute", monitor.post_execute_hook)
+    ip.events.register("pre_run_cell", monitor.pre_run_cell)
+    ip.events.register("post_execute", monitor.post_execute_hook)
