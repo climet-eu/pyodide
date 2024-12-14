50353270626c3957940f8eaca978b71ea00adde4
Juniper Tyree <50025784+juntyr@users.noreply.github.com>
2024-12-14 20:07:18 +0000

    Provide explicit stdio overrides for Pyodide

--- packages/jupyterlite-preload/jupyterlite-preload/jupyterlite_preload.py
+++ packages/jupyterlite-preload/jupyterlite-preload/jupyterlite_preload.py
@@ -3,70 +3,133 @@ import importlib
 import sys
 from pathlib import Path
 
+import js
 import pyodide
 import pyodide_js
 
 import ipyloglite
 
 import pyodide_http
-pyodide_http.patch_all()
 
 
-async def asyncio_gather(*coros_or_futures, return_exceptions=False):
-    results = []
+def patch_syncifiable_asyncio():
+    async def asyncio_gather(*coros_or_futures, return_exceptions=False):
+        results = []
 
-    for coro in coros_or_futures:
-        try:
-            results.append(await coro)
-        except Exception as err:
-            if return_exceptions:
-                results.append(err)
-            else:
-                raise
+        for coro in coros_or_futures:
+            try:
+                results.append(await coro)
+            except Exception as err:
+                if return_exceptions:
+                    results.append(err)
+                else:
+                    raise
 
-    return results
+        return results
 
+    # FIXME: somehow detect if we're actually running on a loop and use the
+    #        actual async sleep in that case
+    async def asyncio_sleep(delay, result=None):
+        import time
 
-# FIXME: somehow detect if we're actually running on a loop and use the actual
-#        async sleep in that case
-async def asyncio_sleep(delay, result=None):
-    import time
-    time.sleep(delay)
-    return result
+        time.sleep(delay)
+        return result
 
-
-asyncio.gather = asyncio_gather
-asyncio.sleep = asyncio_sleep
-
-
-async def loadPackagesFromImports(
-    code: str, options=dict(checkIntegrity=True),
-):
-    imports = set()
-
-    for name in pyodide.code.find_imports(code):
-        if name in sys.modules:
-            continue
-
-        try:
-            spec = importlib.util.find_spec(name)
-        except ModuleNotFoundError:
-            spec = None
-        if spec is not None and Path(spec.origin).parts[:2] == (
-            "/", "drive",
-        ):
-            with open(spec.origin, "r") as f:
-                await loadPackagesFromImports(f.read(), options=options)
-
-        if name in pyodide_js._api._import_name_to_package_name:
-            imports.add(
-                pyodide_js._api._import_name_to_package_name[name]
-            )
-
-    return await pyodide_js.loadPackage(list(imports))
+    asyncio.gather = asyncio_gather
+    asyncio.sleep = asyncio_sleep
 
 
-pyodide_js.loadPackagesFromImports = loadPackagesFromImports
+def patch_pyodide_stdio():
+    pyodideStdout = pyodide.code.run_js(r"""
+    function pyodideStdout(message) {
+        console.log("[pyodide]:", message);
+    }
+    { batched: pyodideStdout }
+    """)
+    pyodideStderr = pyodide.code.run_js(r"""
+    function pyodideStderr(message) {
+        console.err("[pyodide]:", message);
+    }
+    { batched: pyodideStderr }
+    """)
+
+    pyodide_js.setStdout(pyodideStdout)
+    pyodide_js.setStderr(pyodideStderr)
+
+
+def patch_pyodide_load_package():
+    loadPackageMessage = pyodide.code.run_js(r"""
+    function loadPackageMessage(message) {
+        console.log("[micropip]:", message);
+    }
+    loadPackageMessage
+    """)
+    loadPackageError = pyodide.code.run_js(r"""
+    function loadPackageError(message) {
+        console.err("[micropip]:", message);
+    }
+    loadPackageError
+    """)
+
+    _loadPackage = pyodide_js.loadPackage
+
+    async def loadPackage(names, options=None):
+        if options is None:
+            options = js.Object.new()
+
+        if getattr(options, "messageCallback", None) is None:
+            options.messageCallback = loadPackageMessage
+        if getattr(options, "errorCallback", None) is None:
+            options.errorCallback = loadPackageError
+        if getattr(options, "checkIntegrity", None) is None:
+            options.checkIntegrity = True
+
+        return _loadPackage(names, options)
+
+    pyodide_js.loadPackage = loadPackage
+
+    async def loadPackagesFromImports(
+        code: str,
+        options=None,
+    ):
+        if options is None:
+            options = js.Object.new()
+
+        imports = set()
+
+        for name in pyodide.code.find_imports(code):
+            if name in sys.modules:
+                continue
+
+            try:
+                spec = importlib.util.find_spec(name)
+            except ModuleNotFoundError:
+                spec = None
+            if spec is not None and Path(spec.origin).parts[:2] == (
+                "/",
+                "drive",
+            ):
+                with open(spec.origin, "r") as f:
+                    await loadPackagesFromImports(f.read(), options=options)
+
+            if name in pyodide_js._api._import_name_to_package_name:
+                imports.add(pyodide_js._api._import_name_to_package_name[name])
+
+        return await loadPackage(list(imports), options=options)
+
+    pyodide_js.loadPackagesFromImports = loadPackagesFromImports
+
+
+def patch_all():
+    if getattr(patch_all, "_patched", False):
+        return
+    patch_all._patched = True
+
+    pyodide_http.patch_all()
+
+    patch_syncifiable_asyncio()
+    patch_pyodide_stdio()
+    patch_pyodide_load_package()
 
 
 class PyodideMemoryMonitor:
@@ -91,10 +154,13 @@ class PyodideMemoryMonitor:
 
         if memory_after != memory_before:
             print(
-                f"[pyodide]: Memory usage has grown to {memory_after} " +
-                f"(from {memory_before}) for this notebook",
+                f"[pyodide]: Memory usage has grown to {memory_after} "
+                + f"(from {memory_before}) for this notebook",
                 file=sys.stderr if self.memory > 2**30 else sys.stdout,
             )
+
+
+patch_all()
 
 
 def _finalize_with_ipython(ip):
