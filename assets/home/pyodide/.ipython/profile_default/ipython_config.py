440fefc9c71d2c48b8968e44692426eea1caf7b8
Juniper Tyree <50025784+juntyr@users.noreply.github.com>
2025-03-14 07:46:40 +0000

    Switch to using IPython extensions for bootstrap

---
+++ assets/home/pyodide/.ipython/profile_default/ipython_config.py
@@ -0,0 +1,7 @@
+import sys
+from pathlib import Path
+
+sys.path.append(Path(__file__).parent / "startup_extensions")
+
+c.InteractiveShellApp.reraise_ipython_extension_failures = True
+c.InteractiveShellApp.extensions.append("climet_lab_bootstrap")
